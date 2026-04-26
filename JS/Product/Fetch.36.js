d270a3747c5151a3ce8b8a466f90544cac5c3c6f
Mohamed Adel <m7md20051968@gmail.com>
2026-04-26 09:15:33 +0300

    Update and rename Fetch.35.js to Fetch.36.js

--- JS/Product/Fetch.36.js
+++ JS/Product/Fetch.36.js
@@ -13,7 +13,7 @@
         return str.replace(/\|/g, " - ").trim();
     };
 
-async function loadMap() {
+    async function loadMap() {
         try {
             const cache = await caches.open(CACHE_NAME);
             const url = `${BASE_URL}General/map.json`;
@@ -56,16 +56,11 @@ async function loadMap() {
             const cache = await caches.open(CACHE_NAME);
 
             let res = await cache.match(url);
-            const now = Date.now();
-            const threeHours = 3 * 60 * 60 * 1000;
-
-            const lastCacheUpdate = localStorage.getItem("last_feed_update_" + feedFileName);
             
-            if (!res || !lastCacheUpdate || (now - lastCacheUpdate > threeHours)) {
-                res = await fetch(url + "?v=" + now);
+            if (!res) {
+                res = await fetch(url);
                 if (res.ok) {
                     cache.put(url, res.clone());
-                    localStorage.setItem("last_feed_update_" + feedFileName, now);
                 }
             }
 
@@ -75,7 +70,7 @@ async function loadMap() {
             const stride = 32;
 
             for (let i = 0; i < buffer.byteLength; i += stride) {
-                    if (view.getBigUint64(i, true) === targetUID) {
+                if (view.getBigUint64(i, true) === targetUID) {
                     const recordIndex = i / stride;
                     window.currentRecordIndex = recordIndex;
                     const flags = view.getUint8(i + 31);
